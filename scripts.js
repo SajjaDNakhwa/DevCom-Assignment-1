53e259ab952420ca52e3cb1b885a687030a6ff99
ndstab <106055466+SajjaDNakhwa@users.noreply.github.com>
2024-01-28 19:11:44 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,7 @@
+//funtion to greet in different languages when hovered
 const button = document.querySelector('.greeting');
 button.addEventListener('mouseover', () => {
-  var greet = ["Greetings!","Hóla!","Namaste!","Marhaba!","Vanakkam!"];
+  var greet = ["Greetings!","Hóla!","Namaste!","Marhaba!","Vanakkam!","Nǐ Hǎo!","Bonjour!","Konnichiwa!","Ciao!","Namaskaar!"];
   var ran_greet = greet[Math.floor(Math.random()*greet.length)];
   button.innerHTML= ran_greet;
-});
-button.addEventListener('mouseout', () => {
-  var greet = ["Nǐ Hǎo!","Bonjour!","Konnichiwa!","Ciao!","Namaskaar!"]
-  var ran_greet = greet[Math.floor(Math.random()*greet.length)];
-  button.innerHTML = ran_greet;
 });
